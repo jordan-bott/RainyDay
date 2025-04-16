eb5db9aec70e8221cf7bf609f73784ef60810cb1
jordan-bott <jordanbott.dev@gmail.com>
2025-04-16 10:23:12 -0700

    home page initial styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return <div>Welcome to Rainy Day!</div>
+  return (
+    <div className="flex h-full w-full place-items-center place-content-center">
+      <div className="h-140 w-160 bg-lightFG dark:bg-darkFG shadow-blue rounded-lg mb-20 flex flex-col place-items-center place-content-center">
+        Welcome to rainy day!
+      </div>
+    </div>
+  )
 }
